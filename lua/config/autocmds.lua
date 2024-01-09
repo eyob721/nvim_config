62b5133e506c86e0a130dcd3f292a06b17d7b476
eyob721 <eyobmelkamu721@gmail.com>
2024-01-09 19:45:43 +0300

    feat: add autocmds for indentation settings

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,8 @@
+-- Auto group template function
+local function augroup(name)
+    return vim.api.nvim_create_augroup("papi_" .. name, { clear = true })
+end
+
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
@@ -7,4 +12,26 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
   group = highlight_group,
   pattern = '*',
+})
+
+-- [[ Indentation for C and C++ ]]
+vim.api.nvim_create_autocmd({ "BufEnter" }, {
+    group = augroup("c_files"),
+    pattern = { "*.c", "*.h" },
+    callback = function()
+        vim.opt_local.tabstop = 4
+        vim.opt_local.softtabstop = 4
+        vim.opt_local.expandtab = false
+    end,
+})
+
+-- [[ Indentation for files that use 2 tabstops ]]
+vim.api.nvim_create_autocmd({ "BufEnter" }, {
+    group = augroup("web_files"),
+    pattern = { "*.lua", "*.md", "*.html", "*.css", "*.js", "*.sql" },
+    callback = function()
+        vim.opt_local.tabstop = 2
+        vim.opt_local.softtabstop = 2
+        vim.opt_local.expandtab = true
+    end,
 })
