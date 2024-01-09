144d43203e1347fd987676e9ed7e4c36423325b9
eyob721 <eyobmelkamu721@gmail.com>
2024-01-09 16:57:30 +0300

    feat: install syntax highlighting plugin (nvim-treesitter)

---
+++ lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,124 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+    },
+    config = function () 
+        vim.defer_fn(function()
+            require('nvim-treesitter.configs').setup {
+                -- Add languages to be installed here that you want installed for treesitter
+                ensure_installed = {
+                    "awk",
+                    "bash",
+                    "c",
+                    "cpp",
+                    "cmake",
+                    "comment",
+                    "doxygen",
+                    "diff",
+                    "html",
+                    "css",
+                    "scss",
+                    "sql",
+                    "javascript",
+                    "jsdoc",
+                    "json",
+                    "jsonc",
+                    "lua",
+                    "luadoc",
+                    "luap",
+                    "markdown",
+                    "markdown_inline",
+                    "python",
+                    "query",
+                    "regex",
+                    "toml",
+                    "tsx",
+                    "typescript",
+                    "vim",
+                    "vimdoc",
+                    "yaml",
+                    "regex",
+                },
+                autotag = {
+                    enable = true,
+                    enable_rename = true,
+                    enable_close = true,
+                    enable_close_on_slash = true,
+                    filetypes = { "html", "xml" },
+                },
+                -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
+                auto_install = true,
+                -- Install languages synchronously (only applied to `ensure_installed`)
+                sync_install = true,
+                -- List of parsers to ignore installing
+                ignore_install = {},
+                -- You can specify additional Treesitter modules here: -- For example: -- playground = {--enable = true,-- },
+                modules = {},
+                highlight = {
+                    enable = true,
+                    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+                    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+                    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+                    -- Instead of true it can also be a list of languages
+                    additional_vim_regex_highlighting = false,
+                },
+                indent = { enable = true },
+                incremental_selection = {
+                    enable = true,
+                    keymaps = {
+                        init_selection = '<c-space>',
+                        node_incremental = '<c-space>',
+                        scope_incremental = '<c-s>',
+                        node_decremental = '<M-space>',
+                    },
+                },
+                textobjects = {
+                    select = {
+                        enable = true,
+                        lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+                        keymaps = {
+                            -- You can use the capture groups defined in textobjects.scm
+                            ['aa'] = '@parameter.outer',
+                            ['ia'] = '@parameter.inner',
+                            ['af'] = '@function.outer',
+                            ['if'] = '@function.inner',
+                            ['ac'] = '@class.outer',
+                            ['ic'] = '@class.inner',
+                        },
+                    },
+                    move = {
+                        enable = true,
+                        set_jumps = true, -- whether to set jumps in the jumplist
+                        goto_next_start = {
+                            [']m'] = '@function.outer',
+                            [']]'] = '@class.outer',
+                        },
+                        goto_next_end = {
+                            [']M'] = '@function.outer',
+                            [']['] = '@class.outer',
+                        },
+                        goto_previous_start = {
+                            ['[m'] = '@function.outer',
+                            ['[['] = '@class.outer',
+                        },
+                        goto_previous_end = {
+                            ['[M'] = '@function.outer',
+                            ['[]'] = '@class.outer',
+                        },
+                    },
+                    swap = {
+                        enable = true,
+                        swap_next = {
+                            ['<leader>a'] = '@parameter.inner',
+                        },
+                        swap_previous = {
+                            ['<leader>A'] = '@parameter.inner',
+                        },
+                    },
+                },
+            }
+        end, 0)
+    end
+}
