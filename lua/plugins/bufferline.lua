4bb860899b34223720cb0ad52e768b26ddd28542
eyob721 <eyobmelkamu721@gmail.com>
2024-01-10 12:59:28 +0300

    feat: add key maps for moving buffers

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -6,15 +6,24 @@ return {
   version = "*",
   dependencies = 'nvim-tree/nvim-web-devicons',
   keys = {
+    -- Pinning and Grouping buffers
     { "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle pin" },
     { "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete non-pinned buffers" },
+
+    -- Closing buffers
     { "<leader>bo", "<Cmd>BufferLineCloseOthers<CR>", desc = "Delete other buffers" },
     { "<leader>br", "<Cmd>BufferLineCloseRight<CR>", desc = "Delete buffers to the right" },
     { "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>", desc = "Delete buffers to the left" },
-    { "<S-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev buffer" },
+
+    -- Switching between buffers
+    { "<S-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Pre) buffer" },
     { "<S-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
-    { "[b", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev buffer" },
-    { "]b", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
+    -- { "[b", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev buffer" },
+    -- { "]b", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
+    
+    -- Moving buffers positions on the buffer line
+    { "<C-h>", "<cmd>BufferLineMovePrev<cr>", { desc = "Move buffer left" }},
+    { "<C-l>", "<cmd>BufferLineMoveNext<cr>", { desc = "Move buffer right" }},
   },
   opts = {
     options = {
