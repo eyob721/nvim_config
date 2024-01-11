cae13e8376ca5e5725b39c84d47d3e6940da7d7a
eyob721 <eyobmelkamu721@gmail.com>
2024-01-11 18:25:48 +0300

    feat: setup completion windows and mapping

--- lua/plugins/lsp/nvim-cmp.lua
+++ lua/plugins/lsp/nvim-cmp.lua
@@ -1,59 +1,85 @@
+--[[
+==============================================================================
+Auto completion
+==============================================================================
+nvim-cmp: a completion plugin for neovim coded in Lua.
+
+When adding sources:
+  1. first include the source plugin repo in the `dependencies` key
+  2. then include the source name in the `sources` key
+--]]
+
+local icons = require("utils.cmp_util").kind_icons
+local window_opts = require("utils.cmp_util").window_opts
+
 return {
-  -- Use <tab> for completion and snippets (supertab)
-  -- first: disable default <tab> and <s-tab> behavior in LuaSnip
-  {
-    "L3MON4D3/LuaSnip",
-    keys = function()
-      return {}
-    end,
+  "hrsh7th/nvim-cmp",
+  dependencies = {
+    "hrsh7th/cmp-emoji",
   },
-  -- then: setup supertab in cmp
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      "hrsh7th/cmp-emoji",
-      "paopaol/cmp-doxygen",
-    },
-    ---@param opts cmp.ConfigSchema
-    opts = function(_, opts)
-      -- Insert source names here
-      table.insert(opts.sources, { name = "emoji" })
-      table.insert(opts.sources, { name = "doxygen" })
-
-      -- Define completion keymaps
-      -- local has_words_before = function()
-      --   unpack = unpack or table.unpack
-      --   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-      --   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-      -- end
-
-      -- local luasnip = require("luasnip")
-      -- local cmp = require("cmp")
-
-      -- opts.mapping = vim.tbl_extend("force", opts.mapping, {
-      --   ["<Tab>"] = cmp.mapping(function(fallback)
-      --     if cmp.visible() then
-      --       cmp.select_next_item()
-      --       -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-      --       -- this way you will only jump inside the snippet region
-      --     elseif luasnip.expand_or_jumpable() then
-      --       luasnip.expand_or_jump()
-      --     elseif has_words_before() then
-      --       cmp.complete()
-      --     else
-      --       fallback()
-      --     end
-      --   end, { "i", "s" }),
-      --   ["<S-Tab>"] = cmp.mapping(function(fallback)
-      --     if cmp.visible() then
-      --       cmp.select_prev_item()
-      --     elseif luasnip.jumpable(-1) then
-      --       luasnip.jump(-1)
-      --     else
-      --       fallback()
-      --     end
-      --   end, { "i", "s" }),
-      -- })
-    end,
-  },
+  opts = function()
+    vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
+    local cmp = require("cmp")
+    local defaults = require("cmp.config.default")()
+    return {
+      completion = {
+        completeopt = "menu,menuone,noinsert,noselect",
+      },
+      snippet = {
+        expand = function(args)
+          require("luasnip").lsp_expand(args.body)
+        end,
+      },
+      mapping = cmp.mapping.preset.insert({
+        ["<C-j>"] = cmp.mapping.select_next_item({}),
+        ["<C-k>"] = cmp.mapping.select_prev_item({}),
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.abort(),
+        ["<CR>"] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ["<S-CR>"] = cmp.mapping.confirm({
+          behavior = cmp.ConfirmBehavior.Replace,
+          select = false,
+        }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ["<C-CR>"] = function(fallback)
+          cmp.abort()
+          fallback()
+        end,
+      }),
+      sources = cmp.config.sources({
+        { name = "nvim_lsp" },
+        { name = "luasnip" },
+        { name = "path" },
+      }, {
+        { name = "buffer" },
+        { name = "emoji" },
+      }),
+      -- My formatting
+      formatting = {
+        fields = { "kind", "abbr", "menu" },
+        format = function(entry, item)
+          -- Kind icons
+          item.kind = string.format("%s", icons[item.kind])
+          item.menu = ({
+            nvim_lsp = "[LSP]",
+            luasnip = "[Snippet]",
+            buffer = "[Buffer]",
+            path = "[Path]",
+          })[entry.source.name]
+          return item
+        end,
+      },
+      window = {
+        completion = cmp.config.window.bordered(window_opts),
+        documentation = cmp.config.window.bordered(window_opts),
+      },
+      experimental = {
+        ghost_text = {
+          hl_group = "CmpGhostText",
+        },
+      },
+      sorting = defaults.sorting,
+    }
+  end,
 }
